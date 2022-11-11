4d14931f64155b4246a69e6a534ef6dc6a87c032
Ricardo <ricardobvil116@gmail.com>
2022-11-11 10:14:22 -0600

    fix error card review

--- client/src/components/Organisms/DashboardSections/Cliente/Content/DashHomeClient.tsx
+++ client/src/components/Organisms/DashboardSections/Cliente/Content/DashHomeClient.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CardReview from '../../../../Molecules/CardReview/CardReview';
-
+const DashHomeClient = () => {
     return(
         <div className="w-tables overflow-hidden ">
             <CardReview/>
